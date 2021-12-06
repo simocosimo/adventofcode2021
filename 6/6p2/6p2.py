a376c2ea05d5844dc002b033c6e5e0514aad3f43
Simone Cosimo <simosx4@gmail.com>
2021-12-06 17:44:05 +0100

    Recursive one, still takes too long lol

--- 6/6p2/6p2.py
+++ 6/6p2/6p2.py
@@ -1,24 +1,34 @@
-from copy import deepcopy
+from collections import Counter
 
+def explore(val, actual_depth, target_depth):
+    cumulative_son = 1
+    #print(f"Called explore() - cum_son: {cumulative_son}")
+    while actual_depth < target_depth:
+        #print(f"val {val}, actual_depth {actual_depth}, target {target_depth}")
+        val -= 1
+        actual_depth += 1
+        local_depth = actual_depth
+        if val < 0: 
+            val = 6
+            #print("Found a son!")
+            cumulative_son += explore(8, actual_depth, target_depth)
+            #print(f"returned - cum_son: {cumulative_son}")
+            actual_depth = local_depth
+    return cumulative_son;
+    
 with open("../input.txt") as f_input:
     delays = list(map(int, f_input.readline().rstrip().split(',')))
-    x = 0
-    target_days = 256
+    c = Counter(delays)
+    delays = list(set(delays))
+    print(delays, c)
+    target_days = 200
+    tot, n = 0, 0
+    
+    print(f"Unique len: {len(delays)}")
+    for i in range(0, len(delays)):
+        n = explore(delays[i], 0, target_days)
+        n *= c[delays[i]]
+        tot += n
+        print(f"Finished counting for value #{i}")
 
-    while x < target_days:
-        to_append = []
-        m = delays[-1] if delays[-1] != 0 else 1
-        if x + m > target_days:
-            #print("Final stop incoming")
-            m = target_days - x
-        #print(f"Before: {delays}")
-        #print(f"Day {x} - First is {m} - len is: {len(delays)}")
-        for i in range(0, len(delays)):
-            delays[i] -= m
-            while delays[i] < 0:
-                delays.append(8 + delays[i] + 1)
-                delays[i] = 6 + (delays[i] + 1)
-        #print(f"After: {delays}\n")
-        x += m
-
-    print(f"Number of fishes after {target_days} days: {len(delays)}")
+    print(f"Number of fishes after {target_days} days: {tot}")
