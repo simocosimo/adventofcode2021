2dfc848eeb48a94561b72e761642c2f01b1e8ccb
Simone Cosimo <simosx4@gmail.com>
2021-12-06 17:46:12 +0100

    :star2: lol this was easy, not so proud of today

--- 6/6p2/6p2.py
+++ 6/6p2/6p2.py
@@ -1,34 +1,19 @@
 from collections import Counter
 
-def explore(val, actual_depth, target_depth):
-    cumulative_son = 1
-    #print(f"Called explore() - cum_son: {cumulative_son}")
-    while actual_depth < target_depth:
-        #print(f"val {val}, actual_depth {actual_depth}, target {target_depth}")
-        val -= 1
-        actual_depth += 1
-        local_depth = actual_depth
-        if val < 0: 
-            val = 6
-            #print("Found a son!")
-            cumulative_son += explore(8, actual_depth, target_depth)
-            #print(f"returned - cum_son: {cumulative_son}")
-            actual_depth = local_depth
-    return cumulative_son;
-    
 with open("../input.txt") as f_input:
     delays = list(map(int, f_input.readline().rstrip().split(',')))
-    c = Counter(delays)
-    delays = list(set(delays))
-    print(delays, c)
-    target_days = 200
-    tot, n = 0, 0
+    f = [Counter(delays)[x] for x in range(0, 9)]
+    target_days = 256
+    tot = 0
+
+    for i in range(0, target_days):
+        f.append(f.pop(0))
+        f[6] += f[8]
     
-    print(f"Unique len: {len(delays)}")
-    for i in range(0, len(delays)):
-        n = explore(delays[i], 0, target_days)
-        n *= c[delays[i]]
-        tot += n
-        print(f"Finished counting for value #{i}")
+    i = 0
+    while i < len(f) - 1:
+        tot += f[i] + f[i+1]
+        i += 2
+    tot += f[-1]
 
     print(f"Number of fishes after {target_days} days: {tot}")
