7257c9e2699331a0722d82035105bddd3929168e
Simone Cosimo <simosx4@gmail.com>
2021-12-07 09:08:35 +0100

    :star2: faster version with gauss!

--- 7/7p2/7p2.py
+++ 7/7p2/7p2.py
@@ -1,4 +1,4 @@
-sommatoria = lambda v: sum(range(v + 1))
+gauss = lambda v: int(v*(v+1)/2)
 
 with open("../input.txt") as f_input:
     hpos = list(map(int, f_input.readline().rstrip().split(',')))
@@ -9,7 +9,7 @@ with open("../input.txt") as f_input:
     for i in range(min(hpos), max(hpos)):
         f = 0
         for x in range(0, len(hpos)):
-            f += sommatoria(abs(hpos[x] - i))
+            f += gauss(abs(hpos[x] - i))
         if min_fuel == -1 or f < min_fuel: 
             min_fuel = f
             min_pos = i
