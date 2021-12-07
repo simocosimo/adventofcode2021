f97d1dd84463a0a01c37b9f873e001b84d6d7c7c
Simone Cosimo <simosx4@gmail.com>
2021-12-07 10:42:59 +0100

    :star2: optimized a little bit

--- 7/7p2/7p2.py
+++ 7/7p2/7p2.py
@@ -1,15 +1,14 @@
-gauss = lambda v: int(v*(v+1)/2)
-
 with open("../input.txt") as f_input:
+    gauss = lambda v: int(v*(v+1)/2)
     hpos = list(map(int, f_input.readline().rstrip().split(',')))
     min_fuel = -1
     min_pos = 0
-    f = 0
 
     for i in range(min(hpos), max(hpos)):
         f = 0
         for x in range(0, len(hpos)):
             f += gauss(abs(hpos[x] - i))
+            if f > min_fuel and i != min(hpos): break
         if min_fuel == -1 or f < min_fuel: 
             min_fuel = f
             min_pos = i
