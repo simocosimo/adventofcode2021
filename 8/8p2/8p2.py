f8322108f5241742f8ca2ac67f49c87db022e12c
Simone Cosimo <simosx4@gmail.com>
2021-12-08 22:07:18 +0100

    :star2: Cleaned a little bit

--- 8/8p2/8p2.py
+++ 8/8p2/8p2.py
@@ -1,22 +1,18 @@
 def findInString(string, tofind):
-    s = list(string)
-    t = list(tofind)
-    for x in t:
-        if x not in s: return False
+    for x in list(tofind):
+        if x not in list(string): return False
     return True
 
-findByVal = lambda mydict, v: list(mydict.keys())[list(mydict.values()).index(v)]
+findByVal = lambda mydict, v: str(list(mydict.keys())[list(mydict.values()).index(v)])
+sortStuff = lambda l: [''.join(sorted(l[i])) for i in range(0, len(l))]
 
 with open("../input.txt") as f_input:
-
     res = 0
     for l in f_input:
         dictN = {}
         r = ''
-        val = l.rstrip().split('|')[1].split()
-        val = [''.join(sorted(val[i])) for i in range(0, len(val))]
-        conn = l.rstrip().split('|')[0].split()
-        conn = [''.join(sorted(conn[i])) for i in range(0, len(conn))]
+        val = sortStuff(l.rstrip().split('|')[1].split())
+        conn = sortStuff(l.rstrip().split('|')[0].split())
         for x in conn:
             if len(x) == 2: dictN[1] = x
             if len(x) == 4: dictN[4] = x
@@ -26,12 +22,10 @@ with open("../input.txt") as f_input:
         conn.sort(key=len)
        
         for i in range(0, 3):
-            c = 0
-            for x in range(3, len(conn)):
-                if findInString(conn[x], conn[i]): c += 1
+            c = sum([1 if findInString(conn[x], conn[i]) else 0 for x in range(3, len(conn))])
             if c == 2: dictN[5] = conn[i]
-            if c == 1: dictN[3] = conn[i]
-            if c == 0: dictN[2] = conn[i]
+            elif c == 1: dictN[3] = conn[i]
+            elif c == 0: dictN[2] = conn[i]
 
         for i in range(3, len(conn)):
             if findInString(conn[i], dictN[3]):
@@ -42,14 +36,10 @@ with open("../input.txt") as f_input:
             if findInString(conn[i], dictN[5]):
                 dictN[6] = conn.pop(i)
                 break
-            i += 1
         
         dictN[0] = conn[3]
         
-        for x in val:
-            r += str(findByVal(dictN, x))
+        for x in val: r += findByVal(dictN, x)
         res += int(r)
+
     print(f"Result is {res}")
-    
-    
-        
