01cc60aae72caa2d12780e48f7df0d196af2e97c
Simone Cosimo <simosx4@gmail.com>
2021-12-08 22:18:53 +0100

    Collapsed everything on 1 line for the memes

--- 8/8p1/8p1.py
+++ 8/8p1/8p1.py
@@ -1,10 +1,5 @@
 with open("../input.txt") as f_input:
-
     res = 0
-    for l in f_input:
-        l = l.rstrip().split('|')[1].split()
-        lens = list(map(lambda n: 1 if len(n) == 2 or len(n) == 4 or len(n) == 3 or len(n) == 7 else 0, l))
-        res += sum(lens)
-    
+    for l in f_input: res += sum(list(map(lambda n: 1 if len(n) == 2 or len(n) == 4 or len(n) == 3 or len(n) == 7 else 0, l.rstrip().split('|')[1].split())))
     print(res)
         
