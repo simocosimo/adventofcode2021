d8ca4b859c958f11762d878ed09644383989220e
Simone Cosimo <simosx4@gmail.com>
2021-12-12 12:39:23 +0100

    :star2: solved 2nd part, 2mins to run but it works

---
+++ 12/12p2/12p2.py
@@ -0,0 +1,50 @@
+from copy import deepcopy
+
+class Node:
+    name = ''
+    visited = 0
+    sons = []
+
+    def __init__(self, name) -> None: 
+        self.name = name
+        self.sons = []
+        self.visited = 0
+    
+    def addSon(self, son) -> None: 
+        if son not in self.sons and son.name != 'start': 
+            self.sons.append(son)
+    def getSons(self) -> list: return self.sons
+    def isEqual(self, target) -> bool: return (self.name == target.name)
+
+def exploreGraph(start, end, actualPath, pathslist, TwiceToken):
+    if start.isEqual(end) and actualPath not in pathslist:
+        pathslist.append(deepcopy(actualPath))
+        return
+    if TwiceToken:
+        if not start.name.isupper() and start.visited == 1:
+            return
+        elif not start.name.isupper() and start.visited == 2:
+            return
+    start.visited += 1
+    if not start.name.isupper() and start.visited == 2:
+        TwiceToken = True
+    for s in start.getSons():
+        actualPath.append(s.name)
+        exploreGraph(s, end, actualPath, pathslist, TwiceToken)
+        actualPath.pop()
+    start.visited -= 1
+    if not start.name.isupper() and start.visited == 1:
+        TwiceToken = False
+
+with open("../input.txt") as f_input:
+    nodes = {}
+    for line in f_input:
+        line = line.rstrip().split('-')
+        if line[0] not in nodes: nodes[line[0]] = Node(line[0])
+        if line[1] not in nodes: nodes[line[1]] = Node(line[1])
+        nodes[line[0]].addSon(nodes[line[1]])
+        nodes[line[1]].addSon(nodes[line[0]])
+
+    pathlist = []
+    exploreGraph(nodes['start'], nodes['end'], [], pathlist, False)
+    print(len(pathlist))
