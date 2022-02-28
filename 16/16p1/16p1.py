a6c63e3e5aad3e2101af5ec737c3254a030310c3
Simone Cosimo <simosx4@gmail.com>
2022-02-28 22:32:28 +0100

    Completed part one with class and tree structure

--- 16/16p1/16p1.py
+++ 16/16p1/16p1.py
@@ -1,28 +1,32 @@
+from treelib import Tree
+
 class Packet:
 
-    def __init__(self, dictionary, packetString='', binary='') -> None:
-        self.packetstring = packetString
-        self.binarypacket = ""
-        self.subpackets = []
+    staticID = 0
+
+    def __init__(self, binary) -> None:
+        self.binarypacket = binary
+        self.subpackets_number = 0
+        self.subpackets_len = 0
         self.value = -1
+        self.length = -1
         # 0 = next 15 bits are lenght of sub packets
         # 1 = next 11 bits are number of sub packets contained
         self.optype = 0
+        Packet.staticID += 1
 
-        for b in self.packetstring: self.binarypacket += dictionary[b]
         self.version = int(self.binarypacket[0:3], 2)
         self.id = int(self.binarypacket[3:6], 2)
         self.optype = int(self.binarypacket[6], 2)
         if self.id == 4: self.type = "literate"
         else: self.type = "operator"
+        self.stringTag = self.type + str(Packet.staticID)
+
+        if self.optype == 1: self.subpackets_number = int(self.binarypacket[7:7+11], 2)
+        else: self.subpackets_len = int(self.binarypacket[7:7+15], 2)
 
         # parsing if literate value
         if self.type == "literate": self.literateParsing()
-    
-        if self.type == "operator":
-            if self.optype == 1:
-                subpn = int(self.binarypacket[7:7+11], 2)
-                
 
     def literateParsing(self):
         exit = False
@@ -33,23 +37,46 @@ class Packet:
             if self.binarypacket[i-1] == '0': exit = True
             else: i += 5
         self.value = int(part, 2)
+        #self.length = (i + 4) + (4 - (i + 4) % 4)
+        self.length = i + 4
                 
+def getBinaryPacket(string, dictionary):
+    binarypacket = ""
+    for b in string: binarypacket += dictionary[b]
+    return binarypacket
 
+def treeBuilding(binarypacket, tree, parent='') -> int:
+    packet = Packet(binarypacket)
+    llen = packet.subpackets_len
+    if parent == '': tree.create_node(tag=packet.stringTag, identifier=packet.stringTag, data=packet)
+    else: tree.create_node(tag=packet.stringTag, identifier=packet.stringTag, data=packet, parent=parent)
+    if packet.type == "literate": return packet.length
+    if packet.optype == 1:
+        localLen = 18
+        for _ in range(packet.subpackets_number): localLen += treeBuilding(binarypacket[localLen:], tree, parent=packet.stringTag)
+    elif packet.optype == 0:
+        localLen = 22
+        while llen > 0:
+            l = treeBuilding(binarypacket[localLen:], tree, parent=packet.stringTag)
+            localLen += l
+            llen -= l
+    return localLen
 
-with open("../test.txt", "r") as f_input:
+with open("../input.txt", "r") as f_input:
     packetstring = f_input.readline().rstrip()
     f_input.readline()
     translation = {}
+    tree = Tree()
 
     for l in f_input.readlines():
         o, t = l.rstrip().split(" = ")
         translation[o] = t
 
-    packetstring = "D2FE28"
-    p = Packet(translation, packetString=packetstring)
-    print(f"Packet string is {p.packetstring}")
-    print(f"Packet binary is {p.binarypacket}")
-    print(f"Packet type: {p.type} - Version: {p.version} - ID: {p.id}")
-    print(f"Packet value is {p.value if p.value != -1 else 'not a literate packet'}")
+    treeBuilding(getBinaryPacket(packetstring, translation), tree)
+    tree.show()
+
+    versionSum = 0
+    for n in tree.all_nodes_itr(): versionSum += n.data.version
+    print(versionSum)
     
     
