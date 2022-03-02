0180ced495bf3379ee0d6c03006ce70668b07d3c
Simone Cosimo <simosx4@gmail.com>
2022-03-02 17:38:25 +0100

    :star2: Very slow, but working, to improve

---
+++ 17/17p2/17p2.py
@@ -0,0 +1,40 @@
+from functools import reduce
+import math
+
+def sumOfNumbers(n): return math.floor(n * (n + 1) / 2)
+
+def checkLanding(x_constraints, y_constraints, vel):
+    global steps
+    max_x = sumOfNumbers(vel[0])
+
+    def functRed(x, y):
+        global steps
+        if x != deltaX: steps += 1
+        return (x + y) if x != deltaX else (x)
+
+    if max_x < x_constraints[0]: return False
+    for deltaX in range(x_constraints[0], x_constraints[1] + 1):
+        steps = 1
+        if (reduce(functRed, range(vel[0], 0, -1)) == deltaX):
+            y = sumOfNumbers(vel[1]) - sumOfNumbers(abs(vel[1] - steps + 1))
+            if y >= y_constraints[0] and y <= y_constraints[1]: return True
+
+            if deltaX == max_x:
+                while y >= y_constraints[0]: 
+                    steps += 1
+                    y = sumOfNumbers(vel[1]) - sumOfNumbers(abs(vel[1] - steps + 1))
+                    if y <= y_constraints[1] and y >= y_constraints[0]: return True
+    return False
+
+with open("../input.txt", "r") as f_input:
+    info = f_input.readline().rstrip()
+    x_constraints = list(map(int, info.split('x=')[1].split(', y=')[0].split('..')))
+    y_constraints = list(map(int, info.split('x=')[1].split(', y=')[1].split('..')))
+    maxY = max(abs(y_constraints[0]), abs(y_constraints[1]))
+    high_vels = 0
+    steps = 1
+
+    for i in range(1, x_constraints[1] + 1):
+        for x in range(-maxY, maxY + 1):
+            if checkLanding(x_constraints, y_constraints, (i, x)): high_vels += 1
+    print(high_vels)
